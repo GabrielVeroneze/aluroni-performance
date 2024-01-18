875b4a5aef771cd0834d899f09964710b0db09a6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-01-18 20:16:12 -0300

    :zap: perf: Memoiza o componente Buscador e o seu ícone

--- src/components/Buscador/index.tsx
+++ src/components/Buscador/index.tsx
@@ -1,10 +1,12 @@
-import { useContext } from 'react'
+import { memo, useContext, useMemo } from 'react'
 import { BuscaContext } from '@/context/Busca/BuscaContext'
 import { CgSearch } from 'react-icons/cg'
 import styles from './Buscador.module.scss'
 
-const Buscador = () => {
+const Buscador = memo(() => {
     const { busca, setBusca } = useContext(BuscaContext)
+
+    const icone = useMemo(() => <CgSearch size={20} color="#4C4D5E" />, [])
 
     return (
         <div className={styles.container}>
@@ -14,9 +16,9 @@ const Buscador = () => {
                 value={busca}
                 onChange={evento => setBusca(evento.target.value)}
             />
-            <CgSearch size={20} color="#4C4D5E" />
+            {icone}
         </div>
     )
-}
+})
 
 export default Buscador
